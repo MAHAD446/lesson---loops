21101f4504cf99f9cde7c269878c103424beaeb3
mahad446 <mahadmubashir43@gmail.com>
2025-01-25 14:26:14 -0800

    activity#1

---
+++ activity#1.py
@@ -0,0 +1,8 @@
+# input an integer value
+n = int(input("enter the number whose sem you want to find:"))
+
+#iterates for n+1 times: i=1 t0 n+1
+for i in range(1, n+1):
+    sum = sum+i
+    print("/n sum =",sum)
+    
